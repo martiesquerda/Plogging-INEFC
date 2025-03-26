685a32ceb5e8bff05fb2ade8b74eb8bc91e0b32b
martiesquerda <97459056+martiesquerda@users.noreply.github.com>
2025-03-26 22:33:39 +0000

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("scroll", () => {
-    let scrollTop = window.scrollY;
-    document.querySelectorAll(".layer").forEach(layer => {
-        let speed = layer.getAttribute("data-speed");
-        layer.style.transform = `translateY(${scrollTop * speed}px)`;
-    });
-});
